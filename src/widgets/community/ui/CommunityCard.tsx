8408c781ea26996365cfca8900091a47856fdc6a
Juhyoungd <jhapoy106@naver.com>
2025-12-15 11:26:59 +0900

    12/15 - [fix]커뮤니티 UI수졍

--- src/widgets/community/ui/CommunityCard.tsx
+++ src/widgets/community/ui/CommunityCard.tsx
@@ -11,13 +11,13 @@ export default function CommunityCard({ title, onClick }: CommunityCardProps) {
     <button
       type="button"
       onClick={onClick}
-      className="bg-secondary mb-50 flex h-300 w-510 flex-col overflow-hidden rounded-2xl text-left shadow-sm transition hover:-translate-y-1 hover:shadow-md"
+      className="bg-secondary flex h-300 flex-col overflow-hidden rounded-2xl text-left shadow-sm transition hover:-translate-y-1 hover:shadow-md"
     >
       {/* 상단 빈 영역 */}
       <div className="bg-foreground-light flex-1 p-4"></div>
 
       {/* 하단 영역 */}
-      <div className="bg-primary flex items-center gap-3 px-4 py-8">
+      <div className="bg-primary flex items-center gap-12 px-4 py-14">
         {/* 왼쪽 프로필 자리 (+ 아이콘 자리) */}
         <div className="bg-accent flex h-30 w-30 items-center justify-center rounded-2xl text-xl font-bold text-white">
           +
