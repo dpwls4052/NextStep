c9e2818f9aad4a4d8f501ffaa43d4aed6d316a78
dpwls4052 <dpwls4052@naver.com>
2026-01-06 09:43:25 +0900

    [chore]_01/06 포인트 내역 내용 변경

--- src/features/user/quest/model/questMeta.ts
+++ src/features/user/quest/model/questMeta.ts
@@ -1,6 +1,6 @@
 export const QUEST_META = {
-  1: { title: '나의 로드맵 공유하기', reward: 50 },
+  1: { title: '게시글 조회하기', reward: 50 },
   2: { title: '인상적인 로드맵 하트 누르기', reward: 50 },
-  3: { title: '댓글 작성하기', reward: 50 },
-  4: { title: '새로운 워크스페이스 만들기', reward: 50 },
+  3: { title: '커뮤니티에 댓글 작성하기', reward: 50 },
+  4: { title: '뉴스에 댓글 작성하기', reward: 50 },
 } as const
